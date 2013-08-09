d5ee6115230055954596d5f9076ac84774138308
patmood <patrick.n.moody@gmail.com>
2013-08-08 23:11:17 -0500

    update twitter oauth callback for heroku

--- app/helpers/oauth.rb
+++ app/helpers/oauth.rb
@@ -1,5 +1,5 @@
 def request_token
-  callback_url = "http://127.0.0.1:9393/oauth/callback"
+  callback_url = "http://vimsnake.herokuapp.com/oauth/callback"
   consumer = OAuth::Consumer.new(ENV['TWITTER_CONSUMER_KEY'],ENV['TWITTER_CONSUMER_SECRET'], :site => "https://api.twitter.com")
   @request_token = consumer.get_request_token(:oauth_callback => callback_url)
 end

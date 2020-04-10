f405774f3148ad73fecbf806991f04964a81583e
patmood <patrick.n.moody@gmail.com>
2020-04-10 13:14:45 -0700

    add crypto

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from 'firebase-functions'
+const admin = require('firebase-admin')
+const CryptoJS = require('crypto-js')
+admin.initializeApp()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const saveScore = functions.https.onCall((data, context) => {
-  const message = context.auth ? `Hello ${context.auth.uid}` : `hello world`
-  return message
+export const processScore = functions.https.onCall((data, context) => {
+  if (!context.auth || !context.auth.uid) return
+
+  const { score } = data
+  const secret = functions.config().score.secret
+
+  const bytes = CryptoJS.AES.decrypt(score, secret)
+  const originalScore = parseInt(bytes.toString(CryptoJS.enc.Utf8))
+
+  return admin
+    .firestore()
+    .collection('users')
+    .doc(context.auth.uid)
+    .update({
+      topScore: {
+        score: originalScore,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      },
+    })
 })

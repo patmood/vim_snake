8b6dc537eb776e3b843e68bd981f3985f0fc723d
Patrick <4031292+patmood@users.noreply.github.com>
2022-09-15 20:54:40 -0700

    working locally

--- src/wasm-loader.ts
+++ src/wasm-loader.ts
@@ -1,22 +1,21 @@
-import { Go } from './wasm-exec-go'
-
 export async function wasmLoader(wasmUrl) {
-  const go = new Go() // Defined in wasm_exec.js
-  var wasm
-
-  if ('instantiateStreaming' in WebAssembly) {
-    WebAssembly.instantiateStreaming(fetch(wasmUrl), go.importObject).then(function (obj) {
-      wasm = obj.instance
-      go.run(wasm)
-    })
-  } else {
-    fetch(wasmUrl)
-      .then((resp) => resp.arrayBuffer())
-      .then((bytes) =>
-        WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
-          wasm = obj.instance
-          go.run(wasm)
-        })
-      )
+  if (!WebAssembly.instantiateStreaming) {
+    // polyfill
+    WebAssembly.instantiateStreaming = async (resp, importObject) => {
+      const source = await (await resp).arrayBuffer()
+      return await WebAssembly.instantiate(source, importObject)
+    }
   }
+
+  // Provided by wasm_exec.js
+  const go = new Go()
+  let mod, inst
+  const result = await WebAssembly.instantiateStreaming(
+    fetch(wasmUrl),
+    go.importObject
+  )
+  mod = result.module
+  inst = result.instance
+
+  return go.run(inst)
 }
